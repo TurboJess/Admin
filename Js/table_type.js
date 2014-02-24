66bb962fe1fae0c88a40ea3c4e6e88bd20b2d824
Jessica Waite <ImJessW@Jessica-Waites-MacBook-Pro.local>
2014-02-24 11:38:10 -0500

    organizing folders

---
+++ Js/table_type.js
@@ -0,0 +1,8 @@
+// Run the script on DOM ready:
+$(function(){
+ $('table').visualize({type: 'line', height: '600px', width: '720px'});
+	// $('table').visualize({type: 'bar', width: '520px'});
+	// $('table').visualize({type: 'area', width: '520px'});
+	// $('table').visualize({type: 'line', width: '520px'});
+	// $('table').visualize({type: 'pie', height: '400px', width: '520px'});
+});
