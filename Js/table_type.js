333781d10731b8cf42fc99a972d83c3d1262847f
Jessica Waite <ImJessW@Jessica-Waites-MacBook-Pro.local>
2014-02-24 14:58:14 -0500

    table hidden

--- Js/table_type.js
+++ Js/table_type.js
@@ -1,8 +1,9 @@
 // Run the script on DOM ready:
 $(function(){
- $('table').visualize({type: 'line', height: '600px', width: '720px'});
-	// $('table').visualize({type: 'bar', width: '520px'});
-	// $('table').visualize({type: 'area', width: '520px'});
-	// $('table').visualize({type: 'line', width: '520px'});
-	// $('table').visualize({type: 'pie', height: '400px', width: '520px'});
+	$('table').visualize({type: 'line', height: '500px', width: '700px'});
+		// $('table').visualize({type: 'bar', width: '520px'});
+		// $('table').visualize({type: 'area', width: '520px'});
+		// $('table').visualize({type: 'line', width: '520px'});
+		// $('table').visualize({type: 'pie', height: '400px', width: '520px'});
+	// $(’table’).visualize().prev().addClass(’accessHide’); 
 });
