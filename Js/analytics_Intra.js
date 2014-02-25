e062aa3d80a41be83fc9f24375fcf1ee9f61a7d3
Jessica Waite <ImJessW@Jessica-Waites-MacBook-Pro.local>
2014-02-25 14:28:45 -0500

    creating walk through for ux

--- Js/analytics_Intra.js
+++ Js/analytics_Intra.js
@@ -2,16 +2,18 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
 
-    $(".show_daily").on("click", function(){
-    	
-    	var dailyView= $.("#daily")
 
-    	dailyView.show()
+
+    // $(".show_daily").on("click", function(){
+
+    // 	var dailyView= $.("#daily")
+
+    // 	dailyView.show()
 
   
 		// $(".subUl").show(); 
 		// $( ".titledUl" ).children().css( "display", "red" );
 
-});
+// });
 
 });
--- Js/table_type.js
+++ Js/table_type.js
@@ -5,5 +5,24 @@ $(function(){
 		// $('table').visualize({type: 'area', width: '520px'});
 		// $('table').visualize({type: 'line', width: '520px'});
 		// $('table').visualize({type: 'pie', height: '400px', width: '520px'});
-	$(’table’).visualize().prev().addClass(’accessHide’); 
+	// $(’table’).visualize().prev().addClass(’accessHide’); 
+
+
+	// $(".show_daily").on("click", function(){
+	// 	$(’table’).visualize().prev().removeClass(’accessHide’); 
+	// 	});
+	// 	console.log("finished")	
+
+
+
+	// $(".show_daily").on("click", function(){
+	// 	console.log("clicked");
+	// 	$(’table’).visualize().prev().removeClass(’accessHide’); 
+	// 	});
+	// 	$('table').show()
+		// $(’table’).visualize().prev().addClass(’accessHide’); 
+
+
+		console.log("finished")		
+
 });
