dce6a4d304a37d52be62bacab672b07b426616ed
Jessica Waite <ImJessW@Jessica-Waites-MacBook-Pro.local>
2014-02-25 12:28:07 -0500

    adding button functionality

--- Js/analytics_Intra.js
+++ Js/analytics_Intra.js
@@ -1,12 +1,12 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-    
 
-    $(".titledUl").on("click", function(){
-    	// show sub List Items
-  //   		var parentUl= $("titleUl")
-  //   		var childUl= $("subUl")
-  //  I will need to change the current funtion to only apply to the child element soon
+
+    $(".show_daily").on("click", function(){
+    	var dailyView= $.("#daily")
+
+    	dailyView.show()
+
   
 		$(".subUl").show(); 
 		// $( ".titledUl" ).children().css( "display", "red" );
