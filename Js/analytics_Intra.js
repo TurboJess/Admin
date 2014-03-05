b7864f41b979c5125a4593d84409bbb8f27036bb
Jessica Waite <ImJessW@Jessica-Waites-MacBook-Pro.local>
2014-03-05 13:41:35 -0500

    got jquery working on Admin

--- Js/analytics_Intra.js
+++ Js/analytics_Intra.js
@@ -1,19 +1,15 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-
-
-
-    // $(".show_daily").on("click", function(){
-
-    // 	var dailyView= $.("#daily")
-
-    // 	dailyView.show()
-
+    $(".titledUl").on("click", function(){
+    	// show sub List Items
+  //   		var parentUl= $("titleUl")
+  //   		var childUl= $("subUl")
+  //  I will need to change the current funtion to only apply to the child element soon
   
-		// $(".subUl").show(); 
+		$(".subUl").show(); 
 		// $( ".titledUl" ).children().css( "display", "red" );
 
-// });
+});
 
 });
